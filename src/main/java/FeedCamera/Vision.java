cf4c229e9493f12e89d9becbce2ac5e4cf5328c1
Eneficial <wetzelhm@students.osd.wednet.edu>
2019-05-16 06:41:44 -0700

    Everything
    I'm sorry

---
+++ src/main/java/FeedCamera/Vision.java
@@ -0,0 +1,294 @@
+package FeedCamera;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfDouble;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfPoint3f;
+import org.opencv.core.Point;
+import org.opencv.core.Point3;
+import org.opencv.core.Rect;
+import org.opencv.core.RotatedRect;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencv.calib3d.*;
+
+import Pipelines.GripPipelineReflectiveTape;
+
+public class Vision 
+{
+	/**
+	 * 
+	 * 
+	 */
+
+	private CameraFeed cameraThread;
+	public Rect   targetRectangleRight = null, targetRectangeLeft = null;
+	private GripPipelineReflectiveTape pipeline = new GripPipelineReflectiveTape();
+	private double centerX, centerY;
+	
+	private final double LEFT_ANGLE_THRESHOLD = -70;
+	private final double RIGHT_ANGLE_THRESHOLD = -10;
+	private boolean contoursPresent;
+	private int contourSize;
+
+	private double offset = 0.0;
+	private double centerXLeft = 0.0, centerXRight = 0.0;
+	private Mat image = null;
+
+	private static double imageWidth;
+	private static double imageHeight;
+	
+
+	// Values in CM Coordinates of Both Targets
+	private static final MatOfPoint3f worldCoordinatesTarget = new MatOfPoint3f();
+
+	private Mat rvec = new Mat();
+	private Mat tvec = new Mat();
+	private Mat rMat = new Mat();
+
+	private double directAngle; // Angle Between Ray between Robot and Target
+	private double perpAngle; // Angle Between Target and Perpendicular
+
+	private Mat cameraMatrix = new Mat(3, 3, CvType.CV_32FC1);
+
+
+	private MatOfPoint2f imageCoordinatesTarget = new MatOfPoint2f();
+
+	private double angleOffset = 0.0;
+	
+	// This variable and method make sure this class is a singleton.
+	
+	public static Vision vision = null;
+	
+	public static Vision getInstance(CameraFeed cameraThread) 
+	{
+		if (vision == null){
+			vision = new Vision(cameraThread);
+			vision.contourSize = 0;
+			vision.contoursPresent = false;
+			vision.centerX = 0.0;
+
+			worldCoordinatesTarget.alloc(4);
+			
+			new Point3(0,0,0); // Center
+			new Point3(10,0,0); //Right Target Center
+			new Point3(-10, 0, 0); // Left Target Center
+			
+			worldCoordinatesTarget.put(0, 0, 0, 0, 0); //Center
+			worldCoordinatesTarget.put(1, 0, 0, 0, 10); //Right
+			worldCoordinatesTarget.put(2, 0, 0, 0, -10); //Left
+			worldCoordinatesTarget.put(3, 0, 0, 5, -10);
+
+			imageWidth = cameraThread.getImageWidth();
+
+		}
+		
+		return vision;
+	}
+	
+	// This is the rest of the class.
+	
+	private Vision(CameraFeed cameraThread) 
+	{
+		this.cameraThread = cameraThread;
+	}
+
+	public boolean contoursPresent(){
+		return contoursPresent;		
+	}
+
+	public double contourSize(){
+		return contourSize;
+	}
+
+	public double centerX(){
+		return centerX;
+	}
+
+	public double distBetweenContours(){
+		return offset;
+	}
+
+	public Rect getRightRect(){
+		return targetRectangleRight;
+	}
+
+	public Rect getLeftRect(){
+		return targetRectangeLeft;
+	}
+
+	public double getAngleOffset(){
+		return angleOffset;
+	}
+
+	public String targetsPresent(){
+		if(contoursPresent){
+			return "Both";
+		}
+		else if(targetRectangeLeft != null){
+			return "Left";
+		}
+		else if(targetRectangleRight != null){
+			return "Right";
+		}
+		else{
+			return "None";
+		}
+	}
+
+	public double getDirectAngle(){
+		return directAngle;
+	}
+
+	public double getPerpAngle(){
+		return perpAngle;
+	}
+
+	public void findContours(){
+		image = cameraThread.getCurrentImage();
+		pipeline.process(image);
+	}
+
+	public void findVisionTargets(){
+		findContours();
+		getContourTargetAngled();
+		if(targetRectangeLeft != null){
+			cameraThread.addRect(targetRectangeLeft);
+		}
+		if(targetRectangleRight != null){
+			cameraThread.addRect(targetRectangleRight);
+		}
+		contourDistanceBox();
+		if(contoursPresent){
+			calculateAnglesSimple();
+		}
+	}
+
+	public void calculateAnglesSimple(){
+		double imageCenter = (imageWidth/2);
+		angleOffset = -(centerX - imageCenter);
+		directAngle = -((centerX - imageCenter) / (imageCenter));
+		System.out.println(directAngle);
+	}
+
+	public void calculateAnglesPNP(){
+		int leftYCenter = (targetRectangeLeft.y + targetRectangeLeft.height) / 2;
+		int rightYCenter = (targetRectangleRight.y + targetRectangleRight.height) / 2;
+		
+		Point left = new Point(centerXLeft, leftYCenter);
+		Point right = new Point(centerXRight, rightYCenter);
+		Point center = new Point(centerX, (leftYCenter + rightYCenter)/2);
+
+		imageCoordinatesTarget.alloc(4);
+
+		imageCoordinatesTarget.put(0, 0, center.x, center.y);
+		imageCoordinatesTarget.put(1, 0, right.x, right.y);
+		imageCoordinatesTarget.put(2, 0, left.x, left.y);
+		imageCoordinatesTarget.put(3, 0, targetRectangeLeft.x, targetRectangeLeft.y);
+
+		// Get Camera Matrix (TODO Camera Calibration)
+		cameraMatrix.put(0, 0, 1);
+		cameraMatrix.put(1, 1, 1);
+
+		Mat intrinsics = Mat.eye(3, 3, CvType.CV_64F);
+        intrinsics.put(0, 0, 1);
+		intrinsics.put(1, 1, 1);
+		
+		MatOfDouble distCoff = new MatOfDouble();
+
+
+		// Get rotation and translation matrix
+		Calib3d.solvePnP(worldCoordinatesTarget, imageCoordinatesTarget , intrinsics , distCoff, rvec, tvec);
+		Calib3d.Rodrigues(rvec, rMat);
+
+		double x = tvec.get(0, 0)[0];
+		double z = tvec.get(2, 0)[0];
+
+		directAngle = Math.atan2(x, z);
+		System.out.println("Angle" + directAngle);
+
+
+	}
+
+
+	public void getContourTarget(){
+		contourSize = pipeline.filterContoursOutput().size();
+		
+		if(contourSize > 1){
+			targetRectangeLeft = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+			targetRectangleRight = Imgproc.boundingRect(pipeline.filterContoursOutput().get(1));
+
+			if(targetRectangeLeft.x > targetRectangleRight.x){
+				Rect temp = targetRectangeLeft;
+				targetRectangeLeft = targetRectangleRight;
+				targetRectangleRight = temp;
+			}
+
+		}
+
+	}
+
+	public void contourDistanceBox(){
+
+		contoursPresent = (targetRectangeLeft != null && targetRectangleRight != null);
+
+		if(contoursPresent){
+			centerXLeft = targetRectangeLeft.x + targetRectangeLeft.width / 2;
+			centerXRight = targetRectangleRight.x + targetRectangleRight.width / 2;
+
+			offset = Math.abs((centerXLeft - centerXRight));
+			centerX = centerXLeft + (offset/2);
+		}
+		else {
+			offset = 0.0;
+			centerX = 0.0;
+		}
+
+		if(offset > 10.0){
+			System.out.println("Contour Distance: " + offset);
+		}
+		else {
+			System.out.println("No Targets");
+		}
+
+	}
+
+	public void getContourTargetAngled(){
+		
+		Mat image = null;
+		RotatedRect rect1 = null;
+
+		int size = pipeline.filterContoursOutput().size();
+
+		if( size > 1){
+			getContourTarget();
+		}
+		else if( size > 0){
+			MatOfPoint2f rect1Points = new MatOfPoint2f(pipeline.filterContoursOutput().get(0).toArray());
+			rect1 = Imgproc.minAreaRect(rect1Points);
+			System.out.println(rect1.angle);
+						
+			if(rect1.angle < RIGHT_ANGLE_THRESHOLD && rect1.angle > LEFT_ANGLE_THRESHOLD){
+				targetRectangleRight = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+				targetRectangeLeft = null;
+				return;
+			}
+			else if(rect1.angle < LEFT_ANGLE_THRESHOLD) {
+				targetRectangeLeft = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+				targetRectangleRight = null;
+				return;
+			}
+
+			
+		}
+
+	}
+
+
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,119 @@
+import java.util.function.Consumer;
+
+import FeedCamera.CameraFeed;
+import FeedCamera.Vision;
+import Pipelines.GripPipelineReflectiveTape;
+import edu.wpi.first.networktables.EntryListenerFlags;
+import edu.wpi.first.networktables.EntryNotification;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+
+public final class Main {
+
+  private static String configFile = "/boot/frc.json";
+  private static CameraFeed cameraThread;
+  private static int team = 4450;
+  private static String tableName = "PiVision";
+  private static GripPipelineReflectiveTape pipeline = new GripPipelineReflectiveTape();
+
+
+  /**
+   * Main.
+   */
+  public static void main(String... args) {
+    
+    // Set config file path
+    if (args.length > 0) {
+      configFile = args[0];
+      System.out.println("Config File");
+    }
+    
+    // Start NetworkTables
+    NetworkTableInstance ntinst = NetworkTableInstance.getDefault();
+    NetworkTable table = ntinst.getTable(tableName);
+    
+    // Intialize Every Table Entry
+    NetworkTableEntry inside_dist = table.getEntry("inner_dist");
+    NetworkTableEntry contoursPresent = table.getEntry("contours_present");
+    NetworkTableEntry contourSize = table.getEntry("contour_size");
+    NetworkTableEntry centerX = table.getEntry("contour_center_x");
+    NetworkTableEntry targetsPresent = table.getEntry("targets_present");
+
+    NetworkTableEntry directAngle = table.getEntry("turn_angle");
+    NetworkTableEntry perpAngle = table.getEntry("turn_angle_complex");
+
+    NetworkTableEntry angleOffset = table.getEntry("angle_offset");
+
+    // Camera Switch
+    NetworkTableEntry cameraVision = table.getEntry("camera_vision");
+
+
+    System.out.println("Setting up NetworkTables client for team " + team);
+    ntinst.startClientTeam(team);
+
+    // Create camera thread instance
+    cameraThread = CameraFeed.getInstance();
+    System.out.println("Ready Instance");
+
+    // Create Vision Object Instance
+    Vision vision = Vision.getInstance(cameraThread);
+    
+    // Set GRIP Pipeline and Start Thread
+    cameraThread.setPipeline(pipeline);
+    cameraThread.setShowContours(false);
+    cameraThread.start();
+
+    //Boolean Target Visible
+    //X and Y of Rectangles
+    //X and Y Contour Center Coordinates
+
+
+    // loop forever
+    for (;;) {
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException ex) {
+        return;
+      }
+
+      vision.findVisionTargets();
+      // Run Vision Method
+      // Update each vision entry
+      
+      /**
+       * contourSize: the number of closed contours found
+       * 
+       * centerX: X Value of coordinate that is the center of the
+       * two contours in the image coordinate system
+       * 
+       * inside dist: the distance between the centers
+       * of the contours
+       * 
+       * contoursPresent: determines if both contours
+       * are visible by the camera
+       */
+
+      contourSize.setDouble(vision.contourSize());
+      centerX.setDouble(vision.centerX()); 
+      
+      inside_dist.setDouble(vision.distBetweenContours());
+      contoursPresent.setBoolean(vision.contoursPresent());
+      targetsPresent.setString(vision.targetsPresent()); 
+
+      directAngle.setDouble(vision.getDirectAngle());
+      perpAngle.setDouble(vision.getPerpAngle());
+      angleOffset.setDouble(vision.getAngleOffset());
+
+      if((int)cameraVision.getDouble(0.0) != cameraThread.getCameraType()){
+        System.out.println("Changing Camera");
+        cameraThread.ChangeCamera((int)cameraVision.getDouble(0.0));
+      }
+      
+      cameraVision.setDouble(cameraThread.getCameraType());
+      
+
+    }
+  }
+}
